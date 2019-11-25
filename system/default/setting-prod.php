975f1a7d01c226c290816ca06e34728927e8f132
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2019-11-25 13:51:18 +0100

    Copy

---
+++ system/default/setting-prod.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class systemSetting
+{
+
+    protected $dsn = "mysql:host=localhost;dbname=aquazg3_ima;port=3306;charset:utf8";
+
+    protected $user = 'aquazg3_ima';
+
+    protected $password = '1Bi27*J^#f[36#F';
+
+}
